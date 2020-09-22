f0574f5b8549377eda1a6afe5f3615ddc79662c3
Colin Wahl <cwahl@awakesecurity.com>
2020-09-21 20:29:00 -0700

    set output of test

--- test/Main.purs
+++ test/Main.purs
@@ -5,6 +5,7 @@ import Prelude
 import Control.Monad.Except.Trans (runExceptT)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import GitHub.Actions.Core as Core
 import GitHub.Actions.ToolCache as ToolCache
 
 main :: Effect Unit
@@ -12,3 +13,4 @@ main = do
   void $ runExceptT do
     _ <- ToolCache.find' { toolName: "my-tool", versionSpec: "12.x" }
     ToolCache.find { toolName: "my-tool", versionSpec: "12.x", arch: Just "armv6" }
+  Core.setOutput { name: "did-it-work", value: "yes" }
