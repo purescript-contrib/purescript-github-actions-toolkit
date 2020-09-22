e22e3aa28a3e46fb98be5aa9c9cfa6d0f0f7b7af
Colin Wahl <cwahl@awakesecurity.com>
2020-09-21 20:54:52 -0700

    have test fail

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Except.Trans (mapExceptT, runExceptT)
+import Control.Monad.Except.Trans (runExceptT)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
@@ -13,10 +13,10 @@ import GitHub.Actions.ToolCache as ToolCache
 main :: Effect Unit
 main = do
   Core.info "Starting test action..."
-  result <- runExceptT $ mapExceptT identity do
+  result <- runExceptT do
     _ <- ToolCache.find' { toolName: "my-tool", versionSpec: "12.x" }
     ToolCache.find { toolName: "my-tool", versionSpec: "12.x", arch: Just "armv6" }
   case result of
-    Left err -> Core.info (message err)
+    Left err -> Core.setFailed (message err)
     Right _ -> Core.info "No errors in find"
   Core.info "Done running test action."
