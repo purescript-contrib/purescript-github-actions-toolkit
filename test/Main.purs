4474eb6d9fb92b8eaa9d9dc105421a2fe026f4e4
Colin Wahl <cwahl@awakesecurity.com>
2020-09-21 20:51:02 -0700

    try to get it to fail

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Except.Trans (runExceptT)
+import Control.Monad.Except.Trans (mapExceptT, runExceptT)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
@@ -13,7 +13,7 @@ import GitHub.Actions.ToolCache as ToolCache
 main :: Effect Unit
 main = do
   Core.info "Starting test action..."
-  result <- runExceptT do
+  result <- runExceptT $ mapExceptT identity do
     _ <- ToolCache.find' { toolName: "my-tool", versionSpec: "12.x" }
     ToolCache.find { toolName: "my-tool", versionSpec: "12.x", arch: Just "armv6" }
   case result of
