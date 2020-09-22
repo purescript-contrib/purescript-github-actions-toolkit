e2ec8eb826529b6f080eaf23f4422d9cfaa30569
Colin Wahl <cwahl@awakesecurity.com>
2020-09-21 20:26:20 -0700

    test should compile...

--- test/Main.purs
+++ test/Main.purs
@@ -3,11 +3,12 @@ module Test.Main where
 import Prelude
 
 import Control.Monad.Except.Trans (runExceptT)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import GitHub.Actions.ToolCache as ToolCache
 
 main :: Effect Unit
 main = do
-  void $ runExceptT
-    ToolCache.find' { toolName: "my-tool", versionSpec: "12.x" }
-    ToolCache.find { toolName: "my-tool", verionSpec: "12.x", arch: Just "armv6" }
+  void $ runExceptT do
+    _ <- ToolCache.find' { toolName: "my-tool", versionSpec: "12.x" }
+    ToolCache.find { toolName: "my-tool", versionSpec: "12.x", arch: Just "armv6" }
