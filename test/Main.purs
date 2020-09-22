b8bdcd03589ad3c6b9bfb0e7eb0bc51e75114bd6
Colin Wahl <cwahl@awakesecurity.com>
2020-09-21 20:23:07 -0700

    test ToolCache.find bindings

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,12 @@ module Test.Main where
 
 import Prelude
 
+import Control.Monad.Except.Trans (runExceptT)
 import Effect (Effect)
+import GitHub.Actions.ToolCache as ToolCache
 
 main :: Effect Unit
-main = pure unit
+main = do
+  void $ runExceptT
+    ToolCache.find' { toolName: "my-tool", versionSpec: "12.x" }
+    ToolCache.find { toolName: "my-tool", verionSpec: "12.x", arch: Just "armv6" }
